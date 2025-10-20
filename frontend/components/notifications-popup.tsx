9b7357192215eafd9968d8a31551867fbe9b9d0e
Ümit Ahmet <135972028+umittahmett@users.noreply.github.com>
2025-10-20 12:59:49 +0300

    feat: add see all link and improve hover styles in notifications dropdown

--- frontend/components/notifications-popup.tsx
+++ frontend/components/notifications-popup.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { BellIcon } from "lucide-react"
 import { useAuth } from "@/lib/auth-context"
+import Link from "next/link"
 
 export function NotificationsPopup() {
   const { notifications } = useAuth()
@@ -22,11 +23,14 @@ export function NotificationsPopup() {
       </DropdownMenuTrigger>
 
       <DropdownMenuContent className="w-56" align="end">
-        <DropdownMenuLabel>Notifications ({notifications?.length})</DropdownMenuLabel>
+        <div className="flex items-center justify-between">
+          <DropdownMenuLabel>Notifications ({notifications?.length})</DropdownMenuLabel>
+          <Link href='/dashboard/notifications' className="text-sm hover:underline mr-2">See all</Link>
+        </div>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           {notifications && notifications.length > 0 ? notifications.map((notification) => (
-            <DropdownMenuItem className="flex flex-col cursor-pointer hover:!bg-gray-900 !text-start relative" key={notification._id}>
+            <DropdownMenuItem className="flex flex-col cursor-pointer dark:hover:bg-gray-900 hover:!bg-gray-100 !text-start relative" key={notification._id}>
               <div className="flex flex-col">
                 <p className="font-semibold">{notification.subject}</p>
                 <p className="text-sm mt-1 text-muted-foreground">{notification.content}</p>
